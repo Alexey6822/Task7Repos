3dfa041c1c6196d7ae12acf96effb5481407ba88
Alexey6822 <bogdanov_1998@list.ru>
2021-08-22 20:56:04 +0700

    Task7
    Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text;
+using System.Globalization;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputText = File.ReadAllText("input.txt", Encoding.Default);
+            string OutputText = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(InputText);
+            File.WriteAllText("output.txt", OutputText, Encoding.GetEncoding("utf-8"));
+        }
+    }
+}
